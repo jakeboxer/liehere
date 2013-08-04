0a3379b37486d65f93eba375d0f4fad65fc1c3bc
Jake Boxer <jake@github.com>
2013-08-04 16:58:58 -0700

    Add specs for StatementsController#create

--- spec/controllers/statements_controller_spec.rb
+++ spec/controllers/statements_controller_spec.rb
@@ -7,4 +7,28 @@ describe StatementsController do
       expect(response).to be_success
     end
   end
+
+  describe 'POST #create' do
+    context "when there's no user" do
+      it "redirects to the new statement form" do
+        post :create, :statement => {:text => 'irrelevant'}
+
+        new_unfinished_user = User.last
+
+        expect(response).to redirect_to(new_user_statement_url(new_unfinished_user))
+      end
+
+      it "creates a new user" do
+        expect do
+          post :create, :statement => {:text => 'irrelevant'}
+        end.to change { User.count }.by(1)
+      end
+
+      it "creates a new statement" do
+        expect do
+          post :create, :statement => {:text => 'irrelevant'}
+        end.to change { Statement.count }.by(1)
+      end
+    end
+  end
 end
