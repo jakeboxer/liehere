38d79adf65299180dd590637136ef14a274f8ff0
Jake Boxer <jake@github.com>
2013-08-04 01:15:11 -0700

    Add skeleton Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liehere::Application.config.secret_key_base = '92a00ec465b8650e6f29c9cf69e0af0828af1471dbb348d693256d43ceefa47e4557da5fa49048227ff5966d1c6f7d7f07487fa183bdce29a5c7384a547a34bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Liehere::Application.config.session_store :cookie_store, key: '_liehere_session'
