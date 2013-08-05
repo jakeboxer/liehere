6843945f7187e3ac23e4f6a1d3efac588c1329a8
Jake Boxer <jake@github.com>
2013-08-04 19:56:46 -0700

    Update feature spec with radio truth toggle

--- spec/features/quiz_create_spec.rb
+++ spec/features/quiz_create_spec.rb
@@ -2,14 +2,18 @@ feature "Creating a quiz" do
   scenario "Creating a quiz without being logged in" do
     visit "/"
 
-    # First page should have the "new statement" form
-    expect(page).to have_css("form.new_statement")
+    # First page should have the "new statement" form without a radio button
+    within('form.new_statement') do
+      expect(page).not_to have_css('input[type=radio]')
+    end
 
     fill_in 'statement[text]', :with => 'first statement'
     click_button 'Create Statement'
 
-    # Second page should also have the "new statement" form
-    expect(page).to have_css("form.new_statement")
+    # Second page should also have the "new statement" form without a radio button
+    within('form.new_statement') do
+      expect(page).not_to have_css('input[type=radio]')
+    end
 
     # Second page should include a list of existing statements, which should
     # include the first statement.
@@ -20,11 +24,37 @@ feature "Creating a quiz" do
     fill_in 'statement[text]', :with => 'second statement'
     click_button 'Create Statement'
 
+    # Third page should have the "new statement" form with radio buttons for
+    # truths and lies.
+    within('form.new_statement') do
+      expect(page).to have_css('input[type=radio][name="statement[truth]"][value=true]')
+      expect(page).to have_css('input[type=radio][name="statement[truth]"][value=false]')
+    end
+
     # List of statements should now include both, and the second should be a
     # lie.
     within('.statements') do
       expect(page).to have_content('(truth) first statement')
       expect(page).to have_content('(lie) second statement')
+    end
+
+    fill_in 'statement[text]', :with => 'third statement'
+    click_button 'Create Statement'
+
+    # List of statements should include the new statement, which defaulted to a
+    # truth.
+    within('.statements') do
+      expect(page).to have_content('(truth) third statement')
+    end
+
+    fill_in 'statement[text]', :with => 'fourth statement'
+    choose 'lie'
+    click_button 'Create Statement'
+
+    # List of statements should include the new statement, which was manually
+    # chosen to be a lie.
+    within('.statements') do
+      expect(page).to have_content('(lie) fourth statement')
     end
   end
 end
